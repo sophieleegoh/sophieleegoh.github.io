5f6e449cea9cbaaec9244b5ad4ca4f0fa2c82adf
Sophie Lee-Goh <sophieleegoh@gmail.com>
2021-01-31 18:24:26 +0800

    colour text on mouse over

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,14 @@
 console.log("hello world");  
+
+function colourText() 
+{
+    var text = document.getElementById("name"); 
+    text.style.color = '#9dd8ff';
+    
+}
+
+function resetText()
+{
+    var text = document.getElementById("name"); 
+    text.style.color = 'black'
+}
