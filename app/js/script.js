498a5c443628e281f6f6e8d7f4b1e229f63dc5a9
Sophie Lee-Goh <sophieleegoh@gmail.com>
2021-02-06 22:13:16 +0800

    updated about page

--- app/js/script.js
+++ app/js/script.js
@@ -14,3 +14,33 @@ function about()
 {
     window.location.href = "/about.html"
 }
+
+
+$(function(){  // $(document).ready shorthand
+    $('.CS').fadeIn('slow');
+  });
+  
+  $(document).ready(function() {
+      console.log("hi")
+      
+      /* Every time the window is scrolled ... */
+      $(window).scroll( function(){
+      
+          /* Check the location of each desired element */
+          $('.Taiwan').each( function(i){
+              
+              var bottom_of_object = $(this).position().top + $(this).outerHeight();
+              var bottom_of_window = $(window).scrollTop() + $(window).height();
+              
+              /* If the object is completely visible in the window, fade it it */
+              if( bottom_of_window > bottom_of_object ){
+                  
+                  $(this).animate({'opacity':'1'},1500);
+                      
+              }
+              
+          }); 
+      
+      });
+      
+  });
