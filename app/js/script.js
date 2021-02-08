64cb608f7be9ac1dd7876d2ed0387e42e60a8340
Sophie Lee-Goh <sophieleegoh@gmail.com>
2021-02-08 20:27:01 +0800

    update meta data

--- app/js/script.js
+++ app/js/script.js
@@ -17,17 +17,29 @@ function about()
 
 
 $(function(){  // $(document).ready shorthand
-    $('.CS').fadeIn('slow');
+    $('.about').fadeIn('slow');
   });
   
   $(document).ready(function() {
-      console.log("hi")
+    $('.About_Body').each( function(i){
+              
+        var bottom_of_object = $(this).position().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+        /* If the object is completely visible in the window, fade it it */
+        if( bottom_of_window > bottom_of_object ){
+            
+            $(this).animate({'opacity':'1'},1500);
+                
+        }
+        
+    }); 
       
       /* Every time the window is scrolled ... */
-      $(window).scroll( function(){
+       $(window).scroll( function(){
       
           /* Check the location of each desired element */
-          $('.Taiwan').each( function(i){
+          $('.About_Body').each( function(i){
               
               var bottom_of_object = $(this).position().top + $(this).outerHeight();
               var bottom_of_window = $(window).scrollTop() + $(window).height();
@@ -41,6 +53,6 @@ $(function(){  // $(document).ready shorthand
               
           }); 
       
-      });
+       });
       
   });
